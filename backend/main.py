b699913240f2b16546c97ff38782e18e6af80329
disha-bose-8 <dishab817@gmail.com>
2026-06-12 22:59:22 +0530

    Fix CORS for Netlify

--- backend/main.py
+++ backend/main.py
@@ -65,7 +65,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174"],
+    allow_origins=["http://localhost:5173", "http://localhost:5174", "https://defectnet.netlify.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
